cd26958d4ad38943d9444d39503801b87113e93d
zhoudawei <zdw0607@gmail.com>
2023-05-05 19:29:21 +0800

    111

--- zero-start/unit01/003_string.py
+++ zero-start/unit01/003_string.py
@@ -1,3 +1,23 @@
 str1 = "hello world"
 str2 = "abcdefghijklmnopqrstuvwxyz"
 str3 = "012345678901234567890123456789"
+
+s1 = "llo"
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
+print(s1 in str1)
+
+print(s1 not in str2)
+
+print(str1 + str2)
+
+print(s1 * 2)
+
+print(str2[0])
+
+print(str3[1:3])
+
+print(str3[0:13:3])
+
+print(str3.count("012", 2, 13))
+print("dwa ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
