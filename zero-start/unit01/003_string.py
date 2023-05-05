be7e087cf92752a325c0b485c4b1bbe005038075
zhoudawei <zdw0607@gmail.com>
2023-05-05 19:59:37 +0800

    1

--- zero-start/unit01/003_string.py
+++ zero-start/unit01/003_string.py
@@ -19,5 +19,7 @@ print(str3[1:3])
 print(str3[0:13:3])
 
 print(str3.count("012", 2, 13))
-print("dwa ")
+print("str3都是数字,", str3.isalnum())
+print("str1都是字母,", str1.isalpha())
+print("")
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
