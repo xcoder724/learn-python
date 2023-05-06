7421b7515a2f8491636a70e729e4d789e6376a7a
zhoudawei <zdw0607@gmail.com>
2023-05-06 15:53:29 +0800

    number

--- zero-start/unit02/003_string.py
+++ zero-start/unit02/003_string.py
@@ -1,6 +1,7 @@
 str1 = "hello world"
 str2 = "abcdefghijklmnopqrstuvwxyz"
 str3 = "012345678901234567890123456789"
+str4 = "1 2 3 4 5 6 7 8 9 0"
 
 s1 = "llo"
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
@@ -12,6 +13,9 @@ print(str1 + str2)
 
 print(s1 * 2)
 
+
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
+print(" * 字符串下标 * \n")
 print(str2[0])
 
 print(str3[1:3])
@@ -19,8 +23,28 @@ print(str3[1:3])
 print(str3[0:13:3])
 
 print(str3.count("012", 2, 13))
+
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
+print(" * 字符串判断 * \n")
 print("str3都是数字,", str3.isalnum())
 print("str1都是字母,", str1.isalpha())
 print("str2都是字母,", str2.isalpha())
-print("")
+
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
+print(" * 字符串常用 * \n")
+print(str1.join("12345"))
+print(str1.join(["1", "2", "3", "4", "5"]))
+
+print(str4.split(" "))
+print(str4.split(" ",1))
+
+print(str4.startswith("1",0, 4))
+
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
+print(" * 作业 * \n")
+
+article = "Beautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\n"
+print(article.count("better"))
+
+
+
