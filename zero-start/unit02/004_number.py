cd5520c35eac6b610853de8ca244273304b1b08e
zhoudawei <zdw0607@gmail.com>
2023-05-06 15:53:13 +0800

    number

---
+++ zero-start/unit02/004_number.py
@@ -0,0 +1,41 @@
+a = 10
+b = 3
+c = 1.5
+π = 3.14
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+
+print(" * 变量类型 * ")
+print("a的类型是：", type(a))
+print("b的类型是：", type(b))
+print("c的类型是：", type(c))
+print("π的类型是：", type(π))
+print("int(π) = ", int(π))
+print("float(a) = ", float(a))
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+
+print(" * 变量计算 * ")
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a // b = ", a // b)
+print("a % b = ", a % b)
+print("-a = ", -a)
+print("+a = ", +a)
+print("abs(-a) = ", abs(-a))
+print("int('10') = ", int("10"))
+print("float('10.234234234') = ", float("10.234234234"))
+print("divmod(a, b) = ", divmod(a, b))
+print("pow(a, b) = ", pow(a, b))
+print("a ** b = ", a ** b)
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+
+print(" * 作业 * ")
+var1 = input("请输入第一个数字：\n")
+var2 = input("请输入第二个数字：\n")
+print("两个数字的和是：", int(var1) + int(var2))
+
+
