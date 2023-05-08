e9811293cc245722baef3c580c88524fa78dce9f
zhoudawei <zdw0607@gmail.com>
2023-05-08 20:02:22 +0800

    1

---
+++ zero-start/unit03/005_mixed_use.py
@@ -0,0 +1,11 @@
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+list1 = ['Wade','James','McGrady']
+list2 = ['热火队','骑士队','火箭队']
+dic = dict(zip(list1, list2))
+print(dic)
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+t = ('a', 1)
+dic = {t: 'hello'}
+print(dic)
+print(dic[('a', 1)])  
