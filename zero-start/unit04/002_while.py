b24e1c26817c2c96b3e6dca764470b452c71d809
zhoudawei <zdw0607@gmail.com>
2023-05-11 17:41:41 +0800

    while

--- zero-start/unit04/002_while.py
+++ zero-start/unit04/002_while.py
@@ -5,3 +5,14 @@ while number < 4:
     number += 1
     
 print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+num2 = 10
+while num2>=1 :
+    print("number is ", num2)
+    num2 -= 1
+    if num2 == 5 :
+        break
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+list1 = [1,2,3,4,5]
+while list1:
+    print(list1.pop())
