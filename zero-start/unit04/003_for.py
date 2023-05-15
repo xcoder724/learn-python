860c1ba4dd24cb78fed340bb6086c3dfc009d16f
zhoudawei <zdw0607@gmail.com>
2023-05-15 15:16:46 +0800

    for

--- zero-start/unit04/003_for.py
+++ zero-start/unit04/003_for.py
@@ -1,5 +1,10 @@
 print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
 list1 = ["rachel","monica","Phoebe","joey"]
 list1.sort()
-for i in list1:
-    print(i.upper())
+for i, item in enumerate(list1):
+    print("序号：",i,", val: ", item)
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+    
+for i, _ in enumerate(list1):
+    print("序号：",i,", val: ", list1[i])
