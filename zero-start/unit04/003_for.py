fd70eb1459e738c339e924da02e91c123ea2e8be
zhoudawei <zdw0607@gmail.com>
2023-05-15 10:17:50 +0800

    for

--- zero-start/unit04/003_for.py
+++ zero-start/unit04/003_for.py
@@ -1,1 +1,5 @@
 print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+list1 = ["rachel","monica","Phoebe","joey"]
+list1.sort()
+for i in list1:
+    print(i.upper())
