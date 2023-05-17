80b7661db6105f041e9bdd04c9b4414b22a7e935
zhoudawei <zdw0607@gmail.com>
2023-05-17 18:17:32 +0800

    1

---
+++ zero-start/unit05/004_open.py
@@ -0,0 +1,2 @@
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+f1 = open("G:\\workspace\\python\\learn-python\\zero-start\\unit05\\test.txt" ,mode="r", encoding="utf-8")
