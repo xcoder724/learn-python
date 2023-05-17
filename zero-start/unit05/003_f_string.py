127112296018fcdf6a482882f465d7b07d554720
zhoudawei <zdw0607@gmail.com>
2023-05-17 16:28:56 +0800

    f-string

--- zero-start/unit05/003_f_string.py
+++ zero-start/unit05/003_f_string.py
@@ -1,1 +1,19 @@
 print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+str1 = f"{1+2}"
+print(str1)
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+str2 = f"{'a'+'b'}"
+print(str2)
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+f"print('Hello World')"
+print(f"print('Hello World')")
+
+
+print("\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
+number = 123.456
+print(f"{number:10}")
+print(f"{number:010}")
+print(f"{number:4f}")
+print(f"{number:.3f}")
